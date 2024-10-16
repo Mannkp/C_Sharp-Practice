1ce2a6fe2f2d224bfc8ae40178e67a927214411a
Mannkp <mannkpandya@gmail.com>
2024-10-16 16:19:06 +0530

    C# Practice Code

---
+++ CSharp_Practice/OOpPractice.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CSharp_Practice
+{
+
+    //abstract (concept) class
+    public abstract class Pet(string name)
+    {
+        public string Name { get; } = name;
+
+        public abstract string MakesNoise();
+    }
+
+
+
+
+    //method 1: old way of doing these..
+
+    public class People
+    {
+        public People(string name, DateOnly DoB) {
+            Name = name;
+            DateOfBirth = DoB;
+        }
+
+        //setting Access Modifiers to private so that below states can be modified only via this class
+
+        private string Name;
+
+        private DateOnly DateOfBirth;
+
+        public void PrintPersonDetails()
+        {
+            Console.WriteLine($"Name is {Name} and Date of Birth is {DateOfBirth}");
+        }
+
+    }
+
+
+
+    //new method
+
+    public class NewPeople(string name, DateOnly DoB)
+    {
+        private string Name { get; } = name;
+
+        private DateOnly DateOfBirth { get; } = DoB;
+
+        public void PrintPersonDetails()
+        {
+            Console.WriteLine($"Name is {Name} and Date of Birth is {DateOfBirth}");
+        }
+
+
+        //people got pets
+        public List<Pet> Pets { get; } = new(); //so new() makes sure people have pets or no pets, but pets do exist not like there is nothing like pets!
+
+    }
+
+
+
+    // here, Dog and Cat class are based on abstract class Pet and the value name coming from Dog/Pet class initialization goes to Pet Class and we can access it through Dog/Cat class as both are based on Pet class and state value was stored on Pets..
+
+    public class Dog(string name) : Pet(name)
+    {
+        public override string MakesNoise()
+        {
+            return "Barks woof woof";   
+        }
+    }
+    
+    public class Cat(string name) : Pet(name)
+    {
+        public override string MakesNoise()
+        {
+            return "Meows meow meow";   
+        }
+    }
+
+
+    
+
+
+}
+
+
+// Abstract is like concept, we create classes based on this concept(abstract) and it shapes our classes we implement.
+// we need to override the methods(function) to define body for them.
---
+++ CSharp_Practice/Program.cs
@@ -0,0 +1,151 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace CSharp_Practice
+{
+    public class MyApp
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello, World!");
+
+            //strings
+            string name = "MANN"; //strings are immutable
+
+            string sentence = $"His name is {name}"; //string Interpolation
+
+            Console.WriteLine(sentence.ToUpper().Trim());
+
+            Console.WriteLine(sentence.Contains(name));
+
+            int a = 0;
+
+            double b = 1.23;
+
+            float c = 1.123f;
+
+            decimal d = 1.2345m;
+
+            //Conditional and Loops
+
+            if (a + b > 0)
+            {
+                Console.WriteLine($"{a}, {b}, {c}");
+            }
+
+            //Consider List<T> as Cup<T>
+
+            var names = new List<string> { "Mann", "Jiya" };
+            names.Add("Bindu");
+            names.Add("Ketan");
+
+            Console.WriteLine("\n\n\n\n\n***********************************");
+
+            foreach (var personName in names)
+            {
+                Console.WriteLine($"Name {names?.IndexOf(personName)}: {personName}");
+            }
+
+            //print first name:
+            Console.WriteLine($"Name: {names[0]}");
+
+            //print last name:
+            Console.WriteLine($"Name: {names[^1]}");
+
+            //print second-last name:
+            Console.WriteLine($"Name: {names[^2]}");
+
+
+            //Arrays in c#
+
+            var newNames = new string[] { "Mann", "Jiya", "Bindu", "Ketan" };
+
+            Console.WriteLine("\n\nNew Names: ");
+            foreach (var personName in newNames)
+            {
+                Console.WriteLine($"{Array.IndexOf(newNames, personName)}. {personName}");
+            }
+
+            //Note: Arrays are Fixed in Length.
+
+            //So to update array, we can create a new array with updated values and append it/assign it to our array!
+
+            newNames = [.. newNames, "Pandya"];
+
+            Console.WriteLine("\n\nUpdated New Names: ");
+            foreach (var personName in newNames)
+            {
+                Console.WriteLine($"{Array.IndexOf(newNames, personName)}. {personName}");
+            }
+
+
+
+
+            //LINQ = Language Integrated Query:
+
+            List<int> scores = [90, 76, 87, 92, 86, 85];
+
+            IEnumerable<int> filteredScores = from score in scores
+                                              where score > 80
+                                              orderby score ascending
+                                              select score;
+
+            List<int> finalScores = filteredScores.ToList();
+
+            Console.WriteLine($"Scores Greater than 80:");
+
+            foreach (int i in finalScores)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+
+            //Queries in c#
+
+            var scoreQuery = scores.Where(s => s > 80).OrderByDescending(s => s);
+
+
+
+
+            //OOPs in c#
+
+            var personMann = new NewPeople("Mann", new DateOnly(2003, 05, 10));
+
+            personMann.Pets.Add(new Dog("Tommy"));
+            personMann.Pets.Add(new Cat("Mimi"));
+
+            var personJiya = new NewPeople("Jiya", new DateOnly(2008, 11, 05));
+
+            var personKetan = new NewPeople("Ketan", new DateOnly(1973, 05, 07));
+
+            var personBindu = new NewPeople("Bindu", new DateOnly(1974, 06, 18));
+            
+            
+
+            List<NewPeople> family = [personMann, personJiya, personBindu, personKetan];
+
+
+
+            Console.WriteLine("\n\n\n\n***********************************************");
+            
+            Console.WriteLine("My Family Details");
+
+            foreach(var person in family)
+            {
+                person.PrintPersonDetails();
+
+                if (person?.Pets?.Count > 0) {
+
+                    foreach (var pet in person?.Pets)
+                    {
+                        Console.WriteLine($"Has Pet Named: {pet?.Name}");
+                        pet?.MakesNoise();
+                    }
+                }
+            }
+
+        }
+    }
+}
